21a76cdaf1e127d56a9c7e1915ea8019e9143800
HalcyonDavid <davidwfallis@googlemail.com>
2016-01-07 20:14:39 -0800

    added shells

--- coromanda/database.py
+++ coromanda/database.py
@@ -1,5 +1,41 @@
+def getname(gid, home=False):
+    """
+    If home is true return name of home team
+    if home id false return name of away team
+    """
+    return 'aaa'
+    
+def getdate(gid):
+    """
+    Returns date of game id in format dd-mm-yy
+    """
+    return 11-11-11
 
+def win(gid):
+    """
+    Returns 1 if the home team won
+    or 0 if the away team won
+    
+    Parameter
+    ---------
+    gid : int 
+    """
+    return 1
 
+def getodds(gid, home=False):
+    """
+    Returns a dictionary mapping the name of website
+    to the payout.
+    ex. {'name': 1.9, # if name will pay out 1.9
+         'name2': 2}
+    """
+    return {'aaaa': 2}
 
+def getgidsfromdate(date):
+    """
+    Returns a list of gids on the date
+    """
+    return [123,124]
+    
 if __name__ == "__main__":
-    print('hello')
+    pass
