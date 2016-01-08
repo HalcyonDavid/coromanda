19fc343e269a0d9fb7e06b1eaadcfcda56d0cd3e
HalcyonDavid <davidwfallis@googlemail.com>
2016-01-07 19:45:07 -0800

    added plotting

--- coromanda/classes.py
+++ coromanda/classes.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
@@ -5,6 +6,13 @@ import yaml
 import database as db
 
 STAT = {'w5': db.w5}
+COMP = {'w5': 'difference'}
+
+def docomparison(a, b, ctype):
+    if ctype == 'difference':
+        return a - b
+    if ctype == 'ratio':
+        return a/b
 
 class StatPlot(object):
     def __init__(self, stat, ids, outfile='txt.yaml'):
@@ -16,15 +24,15 @@ class StatPlot(object):
         statdict = {}
         means = []
         stds = []
-        for i in self.ids
+        for i in self.ids:
             a = STAT[self.stat](i)
             b = STAT[self.stat](i, home=False)
-            diff = a - b
+            comp = docomparison(a, b, COMP[self.stat])
             wl = db.win(i)
             try:
-                statdict[diff].append(wl)
-            else
-                statdict[diff] = [wl]
+                statdict[comp].append(wl)
+            except:
+                statdict[comp] = [wl]
         for key in statdict:
             a = np.array(statdict[key])
             mean = np.mean(a)
@@ -33,12 +41,12 @@ class StatPlot(object):
             stds.append((key, std))
         self.means = means
         self.stds = stds
-
+        return self.means, self.stds
 
     def fit(self, data):
         data = np.array(data)
-        x = data[:0]
-        y = data[:1]
+        x = data[:,0]
+        y = data[:,1]
         z = np.polyfit(x, y, 2)
         return z
 
@@ -50,55 +58,69 @@ class StatPlot(object):
             f.write(yaml.dump(printer, default_flow_style=False))
 
     def saveplot():
+        fig, (axl, axm, axr) = plt.subplots(3, 1)
         x = self.means[:,0]
         y = self.means[:1]
         func = np.poly1d(self.meanfunc)
         xnew = np.linspace(x.min, x.max, 50)
         ynew = func(xnew)
-
-        plt.plot(x,y,'o', xnew, ynew)
+        axl.plot(x,y,'o', xnew, ynew)
+        axl.title(self.stat + '_mean')
+        x2 = self.stds[:,0]
+        y2 = self.stds[:1]
+        func2 = np.poly1d(self.stdfunc)
+        ynew2 = func2(xnew)
+        axm.plot(x,y2,'o', xnew, ynew2)
+        axm.title(self.stat + '_std')
+        axr.fill_between(x, (ynew + ynew2), (ynew - ynew2),
+                         color='k', alpha=0.3)
+        axr.plot(xnew, ynew)
+        plt.tight_layout()
         plt.savefig(self.stat + '.pdf')
 
-    def execute()
+    def execute():
         mean, std = self.loop()
         self.meanfunc = self.fit(mean)
         self.stdfunc = self.fit(std)
-        self.writer('txt.yaml')
+        self.writer(self.outfile)
         self.saveplot()
 
 class Calculate(object):
-
-    def __init__(self, gid):
+    def __init__(self, gid, sfile='txt.yaml', ofile='out.yaml'):
         self.gid = gid
         self.h = db.gett(gid)
         self.a = db.gett(gid, home=False)
         self.date = db.getdate(gid)
+        self.sfile = sfile
+        self.ofile = ofile
 
-    def game(self, gameid, inputf='txt.yaml'):
+    def game(self, inputf=self.sfile):
         with open(inputf, 'r') as f:
             stats = yaml.load(f)
-        means = np.arry()
-        stds = np.array()
+        means = []
+        stds = []
         for s in stats:
             zmean = s['mean']
             zstd = s['std']
             fmean = np.poly1d(np.array(zmean))
             fstd = np.poly1d(np.array(zstd))
 
-            a = STAT[s](gameid)
-            b = STAT[s](gameid, home=False)
-            diff = a-b
-            means.append(fmean(diff))
-            stds.append(fstd(diff))
+            a = STAT[s](self.gid)
+            b = STAT[s](self.gid, home=False)
+            comp = docomparison(a, b, COMP[s])
+            means.append(float(fmean(comp)))
+            stds.append(float(fstd(comp)))
 
+        means = np.array(means)
+        stds = np.array(stds)
         weighted = means * (1 / stds)
         bigsum = np.sum(weighted)
 
         stdsum = np.sum(1 / stds)
-        self.res = float(weighted / stdsum)
+        self.res = float(bigsum / stdsum)
         return res
 
-    def compare(self, ofile='out.yaml'):
+    def compare(self, ofile=self.ofile):
         hodddict = db.getodd(self.gid)
         aodddict = db.getodd(self.gid, home=False)
         hname = max(hodddict, key=hodddict.get)
@@ -122,5 +144,5 @@ class Calculate(object):
                 f.write('****')
             f.write(self.date)
             f.write(self.gid + '\n')
-            f.write(yaml.dump(printer, default_flow_style=False)
+            f.write(yaml.dump(printer, default_flow_style=False))
         return printer
