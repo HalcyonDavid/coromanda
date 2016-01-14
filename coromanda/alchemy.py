61d11b507bb93cc4ae96b2395b83bbd6f93cd588
HalcyonDavid <davidwfallis@googlemail.com>
2016-01-13 21:54:10 -0800

    Update and rename pydata.py to alchemy.py

---
+++ coromanda/alchemy.py
@@ -0,0 +1,15 @@
+from sqlalchemy import *
+
+engine = create_engine('mysql+mysqlconnector://roo:password@localhost:3306/halcyonnhl')
+
+def getinfo(info, gid):
+  # home, away, winner, gametype
+  value = engine.execute('SELECT %s FROM games WHERE gameID=%d' %(info, gid))
+  return str(value)
+
+def getstat(stat, team, gid):
+  if stat == 'w5':
+      values = engine.execute('SELECT COUNT(*) FROM (SELECT * from altgames WHERE team=''%s'' AND gameID < %d ORDER BY gameID DESC LIMIT 5) AS S WHERE result=''%s''' %(team,ID,'W'))
+  else:
+      values = None
+  return values
--- coromanda/pydata.py
+++
@@ -1,3 +0,0 @@
-from sqlalchemy import *
-
-engine = create_engine('mysql+mysqlconnector://roo:password@localhost:3306/halcyonnhl')
