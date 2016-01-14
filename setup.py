15b9a50654a7efe7bc69fbefe35afae843277361
HalcyonDavid <davidwfallis@googlemail.com>
2016-01-13 21:12:44 -0800

    added setup for install

---
+++ setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from validate import __version__
+
+
+setup(
+    name = 'coromanda',
+    version = __version__,
+    author = 'David',
+    author_email = 'davidwfallis@googlemail.com',
+    packages = ['coromanda'],
+    include_package_data = True,
+    scripts = ['bin/update', 'bin/coromanda'],
+    url = '',
+    description = 'Game prediction',
+    long_description = 'Game prediction',
+    install_requires = [
+        'sqlalchmy',
+        'numpy >=1.9.2',
+        'matplotlib >=1.4.3',
+        'pyyaml >=3.11',
+    ],
+)
